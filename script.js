3d63226e4bf2c6fe3c25000c74ec59407fe65d53
Alexander <alexandercryptock@icloud.com>
2023-09-13 17:18:39 -0400

    added start page and end page

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var questionindex = 0
 var correctanswerindex = 0
 var score = 0
-var questionbox = document.getElementById("Question")
+var questionbox = document.getElementById("QuestionBox")
+var questionel = document.getElementById("Question")
 var answer1box = document.getElementById("answer1")
-answer1box.addEventListener("click",chooseanswer)
+answer1box.addEventListener("click", chooseanswer)
 var answer2box = document.getElementById("answer2")
-answer2box.addEventListener("click",chooseanswer)
+answer2box.addEventListener("click", chooseanswer)
+
+var startbox = document.getElementById("startbox")
+var scorebox = document.getElementById("scorebox")
+
 
 
 var questions = [
@@ -38,7 +43,7 @@ var questions = [
 
 ]
 var timer = 60
-    function startTimer() {
+function startTimer() {
     interval = setInterval(() => {
         document.getElementById('timer').textContent = timer
         if (timer <= 0) {
@@ -50,50 +55,59 @@ var timer = 60
 }
 var interval
 
-var startbutton =document.getElementById("start")
- startbutton.addEventListener("click",start)
- function start(){
+var startbutton = document.getElementById("start")
+startbutton.addEventListener("click", start)
+function start() {
     startTimer()
     startbutton.classList.add("hide")
+    scorebox.classList.add("hide")
+    scorebox
     questionindex = 0
     score = 0
     document.getElementById("QuestionBox").classList.remove("hide")
     setquestion()
- }
+}
+// var restartbutton = document.getElementById("restart")
+// restartbutton.addEventListener("click", start)
 
- function setquestion(){
-    questionbox.innerText = questions[questionindex].question
+function setquestion() {
+    questionel.innerText = questions[questionindex].question
     for (let index = 0; index < questions[questionindex].answers.length; index++) {
-        var answerbox = document.getElementById("answer"+(index+1));
+        var answerbox = document.getElementById("answer" + (index + 1));
         //console.log("answer"+(index+1));
         answerbox.innerText = questions[questionindex].answers[index].Text
         if (questions[questionindex].answers[index].correct) {
             correctanswerindex = index
-           
+
         } answerbox.dataset.correct = questions[questionindex].answers[index].correct
     }
- }
+}
 
- function chooseanswer(event){
-    //console.log(event.target.innerText);
+function chooseanswer(event) {
     var correct = event.target.dataset.correct
-    //console.log(correct);
-    if (correct === "true"){
-        score+=10
-        //console.log("right")
-    } else{
-        timer-=5
-        //console.log("wrong")
-    } 
-    if(questionindex >=questions.length-1){
+    if (correct === "true") {
+        score += 10
+    } else {
+        timer -= 5
+    }
+    if (questionindex >= questions.length - 1) {
         end()
-    } else{
+    } else {
         questionindex++
         setquestion()
     }
- }
- function end(){
-    if (questionindex >=questions.length-1) {
+}
+function end() {
+    if (questionindex >= questions.length - 1) {
+        QuestionBox.classList.add("hide")
+        scorebox.classList.remove("hide")
+        document.getElementById("score").innerText = "score: " + score
         
     }
- }
+}
+var submitbutton = document.getElementById("submit")
+submitbutton.addEventListener("click", submitscore)
+var initials
+function submitscore(){
+initials
+}
